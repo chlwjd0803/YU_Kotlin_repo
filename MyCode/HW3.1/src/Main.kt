7281d58c8c41dce295d9c808faaa3f45e178a83b
CJ <chlwjd0803@naver.com>
2024-07-03 22:11:46 +0900

    create HW3.1

--- MyCode/HW3.1/src/Main.kt
+++ MyCode/HW3.1/src/Main.kt
@@ -23,12 +23,12 @@ fun genBigRandIntArray(size: Int, offset: Int): IntArray {
 
 fun printBigArraySample(bigArray: IntArray, per_line: Int, sample_lines: Int) {
     var samcnt = 0
-    for(i in 0 until sample_lines) {
-        if(samcnt == per_line){
-            println()
+    for(i in 0 until per_line*sample_lines) {
+        if(samcnt == per_line) {
             samcnt=0
+            println()
         }
-        print("${bigArray[i]} ")
+        print("%8d".format(bigArray[i]))
         samcnt++
     }
     println("\n. . . . .")
@@ -38,17 +38,53 @@ fun printBigArraySample(bigArray: IntArray, per_line: Int, sample_lines: Int) {
             println()
             samcnt=0
         }
-        print("${bigArray[i]} ")
+        print("%8d".format(bigArray[i]))
         samcnt++
     }
+    println()
 }
 
 fun _partition(array: IntArray, left: Int, right: Int, pivotIndex: Int): Int {
-
+    val pivotValue: Int // pivot value
+    var newPI: Int // new pivot index
+    var temp: Int
+    var i: Int
+    pivotValue = array[pivotIndex]
+    temp = array[pivotIndex]
+    array[pivotIndex] = array[right]
+    array[right] = temp // Move pivot to end
+    newPI = left
+    i = left
+    while (i <= right - 1) {
+        if (array[i] <= pivotValue) {
+            temp = array[i]
+            array[i] = array[newPI]
+            array[newPI] = temp
+            newPI = newPI + 1
+        }
+        i++
+    }
+    // swap array[storeIndex] and array[right]; Move pivot to its final place
+    temp = array[newPI]
+    array[newPI] = array[right]
+    array[right] = temp
+    return newPI
 }
 
 fun _quickSort(array: IntArray, left: Int, right: Int) {
-
+    var pI : Int
+    var newPI : Int // pivot index
+    if (left >= right) {
+        return;
+    }
+    pI = (left + right) / 2;
+    newPI = _partition(array, left, right, pI);
+    if (left < (newPI - 1)) { _quickSort(array, left, newPI - 1);
+    // recursively sort elements on the left of pivotNewIndex
+    }
+    if ((newPI + 1) < right) { _quickSort(array, newPI + 1, right);
+    // recursively sort elements on the right of pivotNewIndex
+    }
 }
 
 fun quickSort(array: IntArray) {
